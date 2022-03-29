dca3469930920cff8e2ae5d8519ccb64fd2a322a
Cheryl Simpson <csimpson@motionencoding.com>
2022-03-28 20:34:03 -0600

    Add project files.

--- Program.cs
+++ Program.cs
@@ -3,8 +3,37 @@ using System;
 
 namespace StereoKitProjectCore
 {
+    // make application than can create multiple shapes of different colors and allows use to clear all of them or some of them.
+
+
+    enum ObjectType
+    {
+        Cube,
+        Ball,
+        Cyclinder
+            // And show the pointer
+            Default.MeshCube.Draw(Default.Material, c.aim.ToMatrix(new Vec3(1, 1, 4) * U.cm), Color.HSV(0, 0.5f, 0.8f).ToLinear());
+    }
+
+    class Entity
+    {
+
+        public Color color;
+        public Model model;
+        public Pose pose;
+        public string name = System.Guid.NewGuid().ToString();
+
+        public void Draw()
+        {
+            UI.Handle(name, ref this.pose, model.Bounds);
+            model.Draw(pose.ToMatrix());
+        }
+    }
+
     class Program
     {
+        static Pose windowPose = new Pose(-.2f, 0, -0.6f, Quat.LookDir(1, 0, 1));
+        static ObjectType objectType = ObjectType.Cube;
 
         static void ShowController(Handed hand)
         {
@@ -43,19 +72,76 @@ namespace StereoKitProjectCore
 
             Hierarchy.Pop();
             Hierarchy.Pop();
-
-            // And show the pointer
-            Default.MeshCube.Draw(Default.Material, c.aim.ToMatrix(new Vec3(1, 1, 4) * U.cm), Color.HSV(0, 0.5f, 0.8f).ToLinear());
         }
 
-        struct Entity
+        static void drawWindow()
         {
-            public Pose pose;
-            public Model model;
-
-            public Entity(Pose pose,Model model) { this.pose = pose; this.model = model; }
+            UI.WindowBegin("Objects", ref windowPose, new Vec2(40, 0) * U.cm, UIWin.Normal);
+            UI.Label("Object To Create:");
+            if (UI.Radio("Cube", objectType == ObjectType.Cube)) { objectType = ObjectType.Cube; }
+            UI.SameLine();
+            if (UI.Radio("Ball", objectType == ObjectType.Ball)) { objectType = ObjectType.Ball; }
+            UI.SameLine();
+            if (UI.Radio("Cyclinder", objectType == ObjectType.Cyclinder)) { objectType = ObjectType.Cyclinder; }
+            if (UI.Button("New"))
+            {
+                Console.WriteLine("Created a new object");
+                CreateNewEntity(objectType);
+            }
+            UI.WindowEnd();
         }
 
+        static System.Collections.Generic.List<Entity> entities = new System.Collections.Generic.List<Entity>();
+
+        static Matrix floorTransform = Matrix.TS(0, -1.5f, 0, new Vec3(30, 0.1f, 30));
+        static Material floorMaterial = new Material(Shader.FromFile("floor.hlsl")) { Transparency = Transparency.Blend };
+
+        static void RenderFloor()
+        {
+            if (SK.System.displayType == Display.Opaque)
+                Default.MeshCube.Draw(floorMaterial, floorTransform);
+        }
+
+        static void Render()
+        {
+            drawWindow();
+            foreach (var e in entities)
+            {
+                e.Draw();
+            }
+        }
+
+        static Model cube = Model.FromMesh(
+            Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
+            Default.MaterialUI);
+        static Model ball = Model.FromMesh(
+            Mesh.GenerateSphere(0.1f),
+            Default.MaterialUI);
+        static Model cyclinder = Model.FromMesh(
+            Mesh.GenerateCylinder(0.1f, 0.2f, Vec3.One),
+            Default.MaterialUI);
+
+        static void CreateNewEntity(ObjectType objectType)
+        {
+            Model model = objectType switch
+            {
+                ObjectType.Cube => cube,
+                ObjectType.Ball => ball,
+                ObjectType.Cyclinder => cyclinder,
+                _ => throw new Exception("Opps!")
+            };
+            Color color = Color.HSV(0, 1, 1);
+            Pose pose = new Pose(0, 0, -0.5f, Quat.Identity);
+
+            var entity = new Entity() { model = model, color = color, pose = pose };
+
+            entities.Add(entity);
+        }
+        //static Pose applyVelocity(int deltaX, int deltaY, int deltaZ, ref Pose p)
+        //{
+        //    Pose nP = new Pose();
+        //    return np;
+        //}
         static void Main(string[] args)
         {
             // Initialize StereoKit
@@ -67,51 +153,23 @@ namespace StereoKitProjectCore
             if (!SK.Initialize(settings))
                 Environment.Exit(1);
 
+
             // Create assets used by the app
-            Entity cube = new Entity()
-            {
-                pose = new Pose(0, 0, -0.5f, Quat.Identity),
-                model = Model.FromMesh(
-                    Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
-                    Default.MaterialUI)
-            };
-            /*
-            Pose cubePose = new Pose(0, 0, -0.5f, Quat.Identity);
-            Model cube = Model.FromMesh(
-                Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
-                Default.MaterialUI);
-            */
-            Entity ball = new Entity()
-            {
-                pose = new Pose(0, 0, 0.5f, Quat.Identity),
-                model = Model.FromMesh(
-                    Mesh.GenerateSphere(0.1f, 8),
-                    Default.MaterialUI)
-            };
-            /*
-            Pose ballPose = new Pose(0, 0, 0.5f, Quat.Identity);
-            Model ball = Model.FromMesh(
-                Mesh.GenerateSphere(0.1f, 8),
-                Default.MaterialUI);
-            */
-
-            Matrix floorTransform = Matrix.TS(0, -1.5f, 0, new Vec3(30, 0.1f, 30));
-            Material floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
-            floorMaterial.Transparency = Transparency.Blend;
-
-
+            
             // Core application loop
             while (SK.Step(() =>
             {
-                if (SK.System.displayType == Display.Opaque)
-                    Default.MeshCube.Draw(floorMaterial, floorTransform);
+                RenderFloor();
 
-                UI.Handle("Cube", ref cube.pose, cube.model.Bounds);
-                cube.model.Draw(cube.pose.ToMatrix());
-                UI.Handle("Ball", ref ball.pose, ball.model.Bounds);
-                ball.model.Draw(ball.pose.ToMatrix(), Color.HSV(0, 0.5f, 0.8f).ToLinear());
-                //ShowController(Handed.Left);
-                //ShowController(Handed.Right);
+                Render();
+
+                ShowController(Handed.Left);
+                ShowController(Handed.Right);
+                //UI.Handle("Cube", ref cubePose, cube.Bounds);
+                //cube.Draw(cubePose.ToMatrix());
+
+                //UI.Handle("Cube2", ref cubePose2, cube.Bounds);
+                //cube.Draw(cubePose2.ToMatrix());
             })) ;
             SK.Shutdown();
         }
